a7c3a2c39642d89a12306ba66e9ddcf05f3cd22c
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-04-18 21:10:58 +0200

    Import cheerio in functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 const axios = require('axios')
+const cheerio = require('cheerio')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions

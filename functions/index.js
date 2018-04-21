625ead0b05e6016d5f14d3565730b35f8ed8e3dc
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-04-21 13:59:32 +0200

    Create http function that exposes addresses by division

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,58 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+exports.addresses = functions.https.onRequest((req, res) => {
+  getAddresses(req.body.division).then(
+    response => {
+      const $ = cheerio.load(response)
+      let addresses = []
+
+      $('.adressentbl tr').each(function (i, elem) {
+        let row = {}
+
+        $(this).find('.even, .odd').each(function (j, elem) {
+          let key
+
+          switch (j) {
+            case 0: key = 'club'
+              break
+            case 1: key = 'place'
+              break
+            case 2: key = 'address'
+              break
+            case 3: key = 'phone'
+              break
+          }
+
+          if (key) {
+            row[key] = $(this).text()
+          }
+        })
+
+        if (row.hasOwnProperty('club')) {
+          addresses.push(row)
+        }
+      })
+
+      return res.status(200).send(addresses)
+    },
+    error => res.status(500).send('Internal Server Error')
+  ).catch(
+    error => res.status(500).send('Internal Server Error')
+  )
+});
+
+function getAddresses(divison) {
+  return axios({
+    method: 'get',
+    url: `http://cbkregio-oost.be/index.php?page=adressen&afdeling=${divison}`,
+    responseType: 'document'
+  }).then(
+    response => response.data,
+    error => error
+  ).catch(error => error)
+}
